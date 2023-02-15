43f304db6d82b3f0b65dff31702459ebbc1e7012
Claéber Felinto <claeber.felinto@iterative.com.br>
2023-02-15 11:43:03 -0300

    Initial commit

---
+++ src/test/java/br/com/bradesco/desafio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bradesco.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
